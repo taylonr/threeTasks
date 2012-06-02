7be71a17c899b34db7766b737df99d9270cbc117
Nate Taylor <taylonr@gmail.com>
2012-06-02 16:29:15 -0500

    undoing classes

--- blackBrass.rb
+++ blackBrass.rb
@@ -4,8 +4,41 @@ require 'mongo_mapper'
 require 'json'
 require 'omniauth'
 require 'omniauth-twitter'
-require 'task'
-require 'user'
+
+class Task
+  include MongoMapper::Document
+
+  before_validation :set_expires
+
+  scope :by_expires, lambda { |date| where(:expires.lt => date)}
+
+  key :description, String
+  key :user_id, ObjectId
+  key :expires, Time
+  key :time_type, Integer
+  key :completed, Boolean
+
+  def set_expires
+    now = Time.new
+
+    if(time_type == 0)
+      self.expires = now.end_of_day()
+    elsif(time_type == 1)
+      self.expires = now.end_of_month()
+    elsif(time_type == 2)
+      self.expires = now.end_of_year()
+    end
+
+  end
+end
+
+class User
+  include MongoMapper::Document
+
+  key :provider, String
+  key :uid, Integer
+  key :username, String
+end
 
 configure :test do
   set :twitterKey => 'Uwk8eu2sGU8HGhzvCK8aA'
--- user.rb
+++
@@ -1,7 +0,0 @@
-class User
-  include MongoMapper::Document
-
-  key :provider, String
-  key :uid, Integer
-  key :username, String
-end
