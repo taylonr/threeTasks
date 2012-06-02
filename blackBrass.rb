7f78f5cdc1b28087742764bd9ab5c33b3e689a21
Nate Taylor <taylonr@gmail.com>
2012-06-02 17:03:15 -0500

    modified callback

--- blackBrass.rb
+++ blackBrass.rb
@@ -92,7 +92,8 @@ end
 
 get '/auth/:provider/callback' do
   auth = request.env['omniauth.auth']
-  user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first() || User.create(:provider => auth["provider"], :uid => auth["uid"])
-  session[:user_id] = user.id
-  redirect '/'
+  user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first()# || User.create(:provider => auth["provider"], :uid => auth["uid"])
+  user.to_json
+  #session[:user_id] = user.id
+  #redirect '/'
 end
