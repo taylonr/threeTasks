8cf838416db174f86d0b01e5582b7457464394f8
Nate Taylor <taylonr@gmail.com>
2012-06-02 16:21:29 -0500

    removing handler class

--- blackBrass.rb
+++ blackBrass.rb
@@ -4,7 +4,8 @@ require 'mongo_mapper'
 require 'json'
 require 'omniauth'
 require 'omniauth-twitter'
-require 'handler'
+require 'task'
+require 'user'
 
 configure :test do
   set :twitterKey => 'Uwk8eu2sGU8HGhzvCK8aA'
@@ -28,4 +29,38 @@ end
 
 use OmniAuth::Builder do
   provider :twitter, settings.twitterKey, settings.twitterSecret
+end
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/tasks' do
+  content_type :json
+  Task.delete_all(:expires => {:$lt => Time.now})
+
+  @task = Task.where(:user_id => session[:user_id]).limit(3).all
+  while @task.length < 3
+    emptyTask = Task.new(:description => "Enter Task", :time_type => 0)
+    emptyTask.save()
+    @task.push(emptyTask)
+  end
+  @task.to_json
+end
+
+put '/tasks/:id' do
+  content_type :json
+  task = Task.find_by_id(params[:id])
+  task.update_attributes!(JSON.parse request.body.read)
+  task.user_id = session[:user_id]
+  task.save()
+  task.to_json
+end
+
+get '/auth/twitter/callback' do
+  auth = request.env['omniauth.auth']
+  user = User.first(:provider => auth["provider"], :uid => auth["uid"]) || User.create(:provider => auth["provider"], :uid => auth["uid"])
+  #auth["user_info"]["name"]
+  session[:user_id] = user.id
+  redirect '/'
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,3 @@
-require './handler.rb'
 require './blackBrass.rb'
 
 run Sinatra::Application
