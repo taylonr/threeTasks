d3342acdbb4b11d1d67056e8ca61baa56eeac80a
Nate Taylor <taylonr@gmail.com>
2012-06-05 23:30:18 -0500

    fixed javascript error

--- public/app.js
+++ public/app.js
@@ -8,94 +8,99 @@
 var Task = Backbone.Model.extend({});
 
 var TaskStore = Backbone.Collection.extend({
-   model: Task,
-    url: '/tasks'
+    model:Task,
+    url:'/tasks'
 });
 
 var Tasks = new TaskStore;
 
-_.templateSettings = {interpolate : /\{\{(.+?)\}\}/g};
+_.templateSettings = {interpolate:/\{\{(.+?)\}\}/g};
 
 var TaskView = Backbone.View.extend({
-   tagName: 'li',
+    tagName:'li',
     template:_.template($("#task-template").html()),
-    events: {
-        'click .taskView': 'editTask',
-        'keypress .edit'  : 'updateOnEnter',
-        'blur .edit'      : 'close',
-        'click .icon-ok': 'completeTask'
+    events:{
+        'click .taskView':'editTask',
+        'keypress .edit':'updateOnEnter',
+        'blur .edit':'close',
+        'click .icon-ok':'completeTask'
     },
 
-    initialize: function(){
+    initialize:function () {
         this.model.bind('change', this.render, this);
         this.model.bind('destroy', this.remove, this);
     },
 
-    render: function(){
+    render:function () {
         this.$el.html(this.template(this.model.toJSON()));
         this.input = this.$('.edit');
-        if(this.model.toJSON().completed)
+        if (this.model.toJSON().completed)
             this.$el.addClass('completed');
 
         return this;
-    } ,
+    },
 
-    editTask: function(){
+    editTask:function () {
         this.$el.addClass('editing');
         this.input.focus();
     },
 
-    close: function() {
+    close:function () {
         var value = this.input.val();
         if (!value) this.clear();
-        this.model.save({description: value, time_type: 0, completed: false});
+        this.model.save({description:value, time_type:0, completed:false});
         this.$el.removeClass("editing");
         this.$el.removeClass('completed');
     },
 
-    completeTask: function() {
-      this.model.save({completed: true});
+    completeTask:function () {
+        this.model.save({completed:true});
         this.$el.addClass('completed');
     },
 
-    updateOnEnter: function(e) {
+    updateOnEnter:function (e) {
         if (e.keyCode == 13) this.close();
     }
 });
 
 var AppView = Backbone.View.extend({
-   el: $('#taskContainer'),
+    el:$('#taskContainer'),
 
-   initialize: function(){
-       Tasks.bind('add', this.addOne, this);
-       Tasks.bind('reset', this.addAll, this);
-       Tasks.bind('all', this.render, this);
+    initialize:function () {
+        Tasks.bind('add', this.addOne, this);
+        Tasks.bind('reset', this.addAll, this);
+        Tasks.bind('all', this.render, this);
 
-       Tasks.fetch();
-   },
+        Tasks.fetch();
+    },
 
-   render: function(){
+    render:function () {
 
-   },
+    },
 
-    addOne: function(task){
-        var view = new TaskView({model: task});
+    addOne:function (task) {
+        var view = new TaskView({model:task});
         this.$("#task-list").append(view.render().el);
 
-    } ,
+    },
 
-    addAll: function(){
+    addAll:function () {
         Tasks.each(this.addOne);
     }
 });
 
-var App= new AppView;
+var App = new AppView;
 
-($('#signout').bind('click', function(){
-    var signout = $.ajax({
-        url: '/signout',
-        type: 'post'
-    });
+(function ($) {
+    $('#signout').bind('click', function () {
+        var signout = $.ajax({
+            url:'/signout',
+            type:'post'
+        });
 
-    signout.success(function(){window.location = '/'});
-}))(jQuery);
+        signout.success(function () {
+            window.location = '/'
+        });
+    })
+})(jQuery);
+
